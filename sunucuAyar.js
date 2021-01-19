0a6ca89488f4f75a04f70a4ccac07fe0d6ecabfe
Sehinsah <77665669+Sehinsah@users.noreply.github.com>
2021-01-19 12:02:07 +0300

    Add files via upload

---
+++ sunucuAyar.js
@@ -0,0 +1,13 @@
+const ayarlar = {
+  guildID: "793304412294611056", // sunucu idsi
+  ownerRole:"800222769395793941", // sunucu sahiplerinin rolü
+  teyitsizRolleri:[""], // kayıt olmayanlara verieln rol
+  jailRolu:"", // cezalılara verilen rol
+  enAltYetkiliRolu:[''], // ilk yetkili permi
+  activity:"Quato💛Xizy", // botun durumu
+  status:"online",
+  invitelink:"https://discord.gg/Scrv9WMP5s", //davet linki
+  symbol:"⛧", // tag
+  voicechannel:"800373351021150219" // botun gireceği ses kanalı
+};
+module.exports = ayarlar;
